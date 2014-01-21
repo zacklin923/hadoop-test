1f7760939ef79404f004a57acbdadc2fe5b7364c
keyki <keyki.kk@gmail.com>
2014-01-21 16:10:57 +0100

    simple oozie workflow with testjob

---
+++ src/main/java/org/oozie/exercise/TestJob.java
@@ -0,0 +1,12 @@
+package org.oozie.exercise;
+
+/**
+ * User: keyki
+ * Date: 1/21/14
+ * Time: 4:07 PM
+ */
+public class TestJob {
+    public static void main(String args[]) {
+        System.out.println("Called with args: " + args);
+    }
+}
