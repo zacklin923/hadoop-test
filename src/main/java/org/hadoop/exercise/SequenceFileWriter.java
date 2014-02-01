c8df6b86a1a3ca40321711b18e2c6adeedf7e4c3
Krisztian Horvath <keyki.kk@gmail.com>
2014-02-01 16:33:59 +0100

    sequence file writer example

---
+++ src/main/java/org/hadoop/exercise/SequenceFileWriter.java
@@ -0,0 +1,38 @@
+package org.hadoop.exercise;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+
+/**
+ * @author keyki
+ */
+public class SequenceFileWriter {
+    public static void main(String[] args) throws IOException {
+        String location = args[0];
+        Configuration configuration = new Configuration();
+
+        Path path = new Path(location);
+        FileSystem fileSystem = FileSystem.get(configuration);
+        if (fileSystem.exists(path)) {
+            fileSystem.delete(path, true);
+        }
+        fileSystem.create(path);
+
+        IntWritable key = new IntWritable();
+        Text value = new Text("alma");
+        SequenceFile.Writer writer = SequenceFile.createWriter(configuration,
+                SequenceFile.Writer.keyClass(IntWritable.class),
+                SequenceFile.Writer.valueClass(Text.class));
+        for (int i = 0; i < 3001; i++) {
+            key.set(i);
+            writer.append(key, value);
+        }
+        writer.close();
+    }
+}
